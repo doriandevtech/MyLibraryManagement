b0be5559ef68ce9fdcde5ad3084effdf3f158e14
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-05-02 20:56:16 +0200

    update GetUser decorator

--- src/users/get-user.decorator.ts
+++ src/users/get-user.decorator.ts
@@ -2,11 +2,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from './user.entity';
 
 export const GetUser = createParamDecorator(
-  (ctx: ExecutionContext, data?: string): User => {
+  (_data, ctx: ExecutionContext): User => {
     const request: Express.Request = ctx.switchToHttp().getRequest();
-    if (data) {
-      return request.user[data];
-    }
     return request.user;
   },
 );
