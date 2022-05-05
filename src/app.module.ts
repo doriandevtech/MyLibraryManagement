d04424a45ef9fab53413aa6c17cc7b595c8beb3f
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-05-06 00:26:00 +0200

    create Books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
