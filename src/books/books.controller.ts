d854e5cda892b53dda193c462e97962aaee813ec
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-05-06 00:46:41 +0200

    add books endpoints

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
+import { BooksService } from './books.service';
+import { UpdateUserDto } from './dto/update-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.booksService.getUsers();
+  }
+
+  @Get('/me')
+  getMe(user: User): Promise<User> {
+    return this.booksService.getUserById(user.id);
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): Promise<User> {
+    return this.booksService.getUserById(id);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() updatedUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.booksService.updateUser(id, updatedUserDto);
+  }
+
+  @Delete(':id')
+  deleteUserById(@Param('id') id: string): Promise<void> {
+    return this.booksService.deleteUserById(id);
+  }
+}
