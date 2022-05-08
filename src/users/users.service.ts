c929447f1ea5645408cfbc0b9c37fe3f8560e3cc
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-05-08 02:52:49 +0200

    add signin to user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,18 +3,21 @@ import {
   InternalServerErrorException,
   Logger,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private jwtService: JwtService;
   ) {}
 
   private logger = new Logger('UserService');
@@ -31,7 +34,18 @@ export class UsersService {
     return user;
   }
 
-  async signin(createUserDto: CreateUserDto): Promise<User> {}
+  async signin(createUserDto: CreateUserDto): Promise<{ accessToken: string }> {
+    const { username, password } = createUserDto;
+    const user = await this.usersRepository.findOneBy({ username });
+
+    if (user && (await bcrypt.compare(password, user.password)) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException("Please check your credentials");
+    }
+  }
 
   async getUsers(): Promise<User[]> {
     try {
